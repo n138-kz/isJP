7c4e16a3eca417ab2f389f8cecbb007c662dd7a2
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-01-13 18:45:56 +0900

    class

--- index.php
+++ index.php
@@ -6,160 +6,164 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, Origin, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-function is_included_ipv4_addresses($range, $remote_ip){
-	/**
-	 * IPアドレスが指定した範囲内にあるかどうか判別する
-	 * https://qiita.com/ran/items/039706c93a8ff85a011a
-	 * 
-	 * @param {String} range CIDR表記(0.0.0.0/32)
-	 * @param {String} remote_ip
-	 * @return {Boolean} 判別結果
-	 *
-	 */
-	list($accept_ip, $mask) = explode('/', $range);
-	$accept_long = ip2long($accept_ip) >> (32 - $mask);
-	$remote_long = ip2long($remote_ip) >> (32 - $mask);
-	return $accept_long == $remote_long;
-}
+class IsJP {
 
-function isJP($reqip){
-	/**
-	 * IPアドレス一覧をダウンロードしてコメント行と空白行を削る
-	 * 
-	 * @param {String} reqip
-	 * @return {Boolean} 判別結果
-	 *
-	 */
-	$ipv4_raw = '';
-	$ipv4 = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16'];
-
-	/* リクエストパラメータ `ip` と比較してマッチしたら TRUE 返答返し終了 */
-	foreach ( $ipv4 as $key => $val ) {
-		if ( is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
-			return ['result'=>FALSE, 'detail'=>'RFC1918'];
-		}
-	}
-
-	$ipv4 = ['127.0.0.0/8'];
-	foreach ( $ipv4 as $key => $val ) {
-		if ( is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
-			return ['result'=>FALSE, 'detail'=>'RFC5735'];
-		}
-	}
-
-	$ipv4 = ['169.254.0.0/16'];
-	foreach ( $ipv4 as $key => $val ) {
-		if ( is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
-			return ['result'=>FALSE, 'detail'=>'RFC3927'];
-		}
-	}
-
-	$internalDB = [];
-	/**
-	 * 
-	 * @flow InternalDB::textfile
-	 * - <キャッシュファイル>が存在するか→[a0]
-	 * →[a0]が真の時、mtimeを取得→[a1]
-	 *    →[a1]の時間がと今の時間を比較→[a2]
-	 *    →[a2]の時間が18時間(3/4日)以上空いているか→[a3]
-	 *    →[a3]が真の時、<神様データ>からデータダウンロードする→[a4]
-	 *       →[a4]を整形し、<キャッシュファイル>に保存。→[a5]
-	 *       →[a5]を保存できたか→[a6]
-	 *       →比較元を<神様データ>とする。
-	 *    →[a3]が偽の時、比較元を<キャッシュファイル>とする。
-	 * →[a0]が偽の時、<神様データ>からデータダウンロードする→[a4] 
-	 *    →[a4]を整形し、<キャッシュファイル>に保存。→[a5]
-	 *    →比較元を<神様データ>とする。
-	 * 
-	 */
-	$internalDB['mode'] = 'text';
-	if (FALSE) {
-	} elseif ( $internalDB['mode'] == 'text' ) {
-		/* InternalDB::textfile */
-		$internalDB['fname'] = 'jp.txt';
-		if (file_exists($internalDB['fname'])) {
-			$internalDB['mtime'] = filemtime($internalDB['fname']);
-			$internalDB['older'] = FALSE;
-			$internalDB['older'] = ( time() - $internalDB['mtime'] ) >= ( 3660 * 18 );
-
-			if ($internalDB['older']) {
-				$ipv4_raw = file_get_contents('https://ipv4.fetus.jp/jp.txt');
-				$internalDB['saved'] = file_put_contents($internalDB['fname'], $ipv4_raw);
-			} else {
-				$ipv4_raw = file_get_contents($internalDB['fname']);
-			}
-		} else {
-			$ipv4_raw = file_get_contents('https://ipv4.fetus.jp/jp.txt');
-			$internalDB['saved'] = file_put_contents($internalDB['fname'], $ipv4_raw);
-		}
-	}
-
-	/* ダウンロードして整形 */
-	$ipv4_raw = file('https://ipv4.fetus.jp/jp.txt');
-	$ipv4 = '';
-	foreach( $ipv4_raw as $key => $val ){
-		$val = trim( $val );
-		$val = str_replace( array("\r\n", "\r", "\n"), '', $val );
-		if( empty( $val ) || preg_match( '/^#/', $val ) ){
-			$val = NULL;
-		}
-
-		if( ! is_null( $val ) ){
-			$ipv4 .= $val . PHP_EOL;
-		}
-	}
-	$ipv4 = trim( $ipv4 );
-	$ipv4 = explode( "\n", $ipv4 );
-	/* ダウンロードして整形::until */
-
-	/* リクエストパラメータ `ip` と比較してマッチしたら TRUE 返答返し終了 */
-	foreach ( $ipv4 as $key => $val ) {
-		if ( is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
-			return ['result'=>TRUE, 'detail'=>'ja_JP'];
-		}
-	}
-
-	return ['result'=>FALSE, 'detail'=>''];
-}
-
-function concat($arr){
-	return implode( '', $arr );
-}
-
-function main(){
-	/* リクエストパラメータ `ip` に値を持ってたらそれに置き換える */
-	$reqip = $_SERVER['REMOTE_ADDR'];
-	if ( isset( $_GET['ip'] ) && $_GET['ip'] != '' ) {
-		$reqip = $_GET['ip'];
+	function is_included_ipv4_addresses($range, $remote_ip){
+		/**
+		 * IPアドレスが指定した範囲内にあるかどうか判別する
+		 * https://qiita.com/ran/items/039706c93a8ff85a011a
+		 * 
+		 * @param {String} range CIDR表記(0.0.0.0/32)
+		 * @param {String} remote_ip
+		 * @return {Boolean} 判別結果
+		 *
+		 */
+		list($accept_ip, $mask) = explode('/', $range);
+		$accept_long = ip2long($accept_ip) >> (32 - $mask);
+		$remote_long = ip2long($remote_ip) >> (32 - $mask);
+		return $accept_long == $remote_long;
 	}
 	
-	echo json_encode(
-		[
-			'meta' => [
-				'version' => 2,
-				'runtime_hash' => md5(md5_file(__FILE__, TRUE)),
-				'runtime_version' => dechex(filemtime(__FILE__)),
-				'issued_at'=>[
-					'timestamp'=>time(),
-					'description'=>date('Y/m/d H:i:s T')
-				]
-			],
-			'result' => [
-				'result'=>isJP($reqip),
-				'request'=>$reqip,
-			],
-			'urls' => [
-				'github_url' => 'https://github.com/n138-kz/isJP',
-				'git_url' => 'git@github.com:n138-kz/isJP.git',
-				'database_url' => 'https://ipv4.fetus.jp/jp.txt',
-			],
-			'usage' => [
-				concat([$_SERVER['REQUEST_SCHEME'], '://', $_SERVER['HTTP_HOST'], preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']), '']),
-				concat([$_SERVER['REQUEST_SCHEME'], '://', $_SERVER['HTTP_HOST'], preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']), '?ip=', $_SERVER['REMOTE_ADDR'], '']),
-			],
-		]
-	, JSON_PRETTY_PRINT | JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_SLASHES);
-	exit();
+	function isJP($reqip){
+		/**
+		 * IPアドレス一覧をダウンロードしてコメント行と空白行を削る
+		 * 
+		 * @param {String} reqip
+		 * @return {Boolean} 判別結果
+		 *
+		 */
+		$ipv4_raw = '';
+		$ipv4 = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16'];
+	
+		/* リクエストパラメータ `ip` と比較してマッチしたら TRUE 返答返し終了 */
+		foreach ( $ipv4 as $key => $val ) {
+			if ( $this->is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
+				return ['result'=>FALSE, 'detail'=>'RFC1918'];
+			}
+		}
+	
+		$ipv4 = ['127.0.0.0/8'];
+		foreach ( $ipv4 as $key => $val ) {
+			if ( $this->is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
+				return ['result'=>FALSE, 'detail'=>'RFC5735'];
+			}
+		}
+	
+		$ipv4 = ['169.254.0.0/16'];
+		foreach ( $ipv4 as $key => $val ) {
+			if ( $this->is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
+				return ['result'=>FALSE, 'detail'=>'RFC3927'];
+			}
+		}
+	
+		$internalDB = [];
+		/**
+		 * 
+		 * @flow InternalDB::textfile
+		 * - <キャッシュファイル>が存在するか→[a0]
+		 * →[a0]が真の時、mtimeを取得→[a1]
+		 *    →[a1]の時間がと今の時間を比較→[a2]
+		 *    →[a2]の時間が18時間(3/4日)以上空いているか→[a3]
+		 *    →[a3]が真の時、<神様データ>からデータダウンロードする→[a4]
+		 *       →[a4]を整形し、<キャッシュファイル>に保存。→[a5]
+		 *       →[a5]を保存できたか→[a6]
+		 *       →比較元を<神様データ>とする。
+		 *    →[a3]が偽の時、比較元を<キャッシュファイル>とする。
+		 * →[a0]が偽の時、<神様データ>からデータダウンロードする→[a4] 
+		 *    →[a4]を整形し、<キャッシュファイル>に保存。→[a5]
+		 *    →比較元を<神様データ>とする。
+		 * 
+		 */
+		$internalDB['mode'] = 'text';
+		if (FALSE) {
+		} elseif ( $internalDB['mode'] == 'text' ) {
+			/* InternalDB::textfile */
+			$internalDB['fname'] = 'jp.txt';
+			if (file_exists($internalDB['fname'])) {
+				$internalDB['mtime'] = filemtime($internalDB['fname']);
+				$internalDB['older'] = FALSE;
+				$internalDB['older'] = ( time() - $internalDB['mtime'] ) >= ( 3660 * 18 );
+	
+				if ($internalDB['older']) {
+					$ipv4_raw = file_get_contents('https://ipv4.fetus.jp/jp.txt');
+					$internalDB['saved'] = file_put_contents($internalDB['fname'], $ipv4_raw);
+				} else {
+					$ipv4_raw = file_get_contents($internalDB['fname']);
+				}
+			} else {
+				$ipv4_raw = file_get_contents('https://ipv4.fetus.jp/jp.txt');
+				$internalDB['saved'] = file_put_contents($internalDB['fname'], $ipv4_raw);
+			}
+		}
+	
+		/* ダウンロードして整形 */
+		$ipv4_raw = file('https://ipv4.fetus.jp/jp.txt');
+		$ipv4 = '';
+		foreach( $ipv4_raw as $key => $val ){
+			$val = trim( $val );
+			$val = str_replace( array("\r\n", "\r", "\n"), '', $val );
+			if( empty( $val ) || preg_match( '/^#/', $val ) ){
+				$val = NULL;
+			}
+	
+			if( ! is_null( $val ) ){
+				$ipv4 .= $val . PHP_EOL;
+			}
+		}
+		$ipv4 = trim( $ipv4 );
+		$ipv4 = explode( "\n", $ipv4 );
+		/* ダウンロードして整形::until */
+	
+		/* リクエストパラメータ `ip` と比較してマッチしたら TRUE 返答返し終了 */
+		foreach ( $ipv4 as $key => $val ) {
+			if ( is_included_ipv4_addresses( $val, $reqip ) === TRUE ) {
+				return ['result'=>TRUE, 'detail'=>'ja_JP'];
+			}
+		}
+	
+		return ['result'=>FALSE, 'detail'=>''];
+	}
+	
+	function concat($arr){
+		return implode( '', $arr );
+	}
+	
+	function main(){
+		/* リクエストパラメータ `ip` に値を持ってたらそれに置き換える */
+		$reqip = $_SERVER['REMOTE_ADDR'];
+		if ( isset( $_GET['ip'] ) && $_GET['ip'] != '' ) {
+			$reqip = $_GET['ip'];
+		}
+		
+		echo json_encode(
+			[
+				'meta' => [
+					'version' => 2,
+					'runtime_hash' => md5(md5_file(__FILE__, TRUE)),
+					'runtime_version' => dechex(filemtime(__FILE__)),
+					'issued_at'=>[
+						'timestamp'=>time(),
+						'description'=>date('Y/m/d H:i:s T')
+					]
+				],
+				'result' => [
+					'result'=>$this->isJP($reqip),
+					'request'=>$reqip,
+				],
+				'urls' => [
+					'github_url' => 'https://github.com/n138-kz/isJP',
+					'git_url' => 'git@github.com:n138-kz/isJP.git',
+					'database_url' => 'https://ipv4.fetus.jp/jp.txt',
+				],
+				'usage' => [
+					$this->concat([$_SERVER['REQUEST_SCHEME'], '://', $_SERVER['HTTP_HOST'], preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']), '']),
+					$this->concat([$_SERVER['REQUEST_SCHEME'], '://', $_SERVER['HTTP_HOST'], preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']), '?ip=', $_SERVER['REMOTE_ADDR'], '']),
+				],
+			]
+		, JSON_PRETTY_PRINT | JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_SLASHES);
+		exit();
+	}
 }
-main();
 
+$api = new isJP();
+$api->main();
